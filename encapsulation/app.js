586c0fd836e60519ef2455b0c3ebf15f9335562e
AlandKadir-667 <30255074@bsdc.ac.uk>
2024-10-25 10:01:03 +0100

    HI

---
+++ encapsulation/app.js
@@ -0,0 +1,27 @@
+console.log("This is running from my web page")
+
+class Person {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    greeting() {
+        console.log(`Hello ${this.name}`);
+    }
+
+    howOld() {
+        console.log(this.name, 'is', this.age, 'years old');
+    }
+
+    howOldPlusDays(days) {
+        const newAge = this.age + Number(days/365);
+        console.log(this.name, 'is', newAge, 'years old in ',days,'daysP');
+    }
+
+}
+
+const a = new Person("Dave", 53, "Male");
+const b = new Person("Aland",19,"They");
+const c = new Person("Ollie",17,"Male");
